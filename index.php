c4c5ce74a8f9a25dfca4d8cba67773c1bcebea7b
Nguyen Dan <nguyendan20022002@gmail.com>
2023-05-23 22:49:32 +0700

    update_link_php

--- index.php
+++ index.php
@@ -99,17 +99,17 @@
                                 <div class="main-menu d-none d-md-block">
                                     <nav>
                                         <ul id="navigation">
-                                            <li><a href="index.html">Home</a></li>
-                                            <li><a href="categori.html">Category</a></li>
-                                            <li><a href="about.html">About</a></li>
-                                            <li><a href="latest_news.html">Latest News</a></li>
-                                            <li><a href="contact.html">Contact</a></li>
+                                            <li><a href="index.php">Home</a></li>
+                                            <li><a href="categori.php">Category</a></li>
+                                            <li><a href="about.php">About</a></li>
+                                            <li><a href="latest_news.php">Latest News</a></li>
+                                            <li><a href="contact.php">Contact</a></li>
                                             <li><a href="#">Pages</a>
                                                 <ul class="submenu">
-                                                    <li><a href="elements.html">Element</a></li>
-                                                    <li><a href="blog.html">Blog</a></li>
-                                                    <li><a href="single-blog.html">Blog Details</a></li>
-                                                    <li><a href="details.html">Categori Details</a></li>
+                                                    <li><a href="elements.php">Element</a></li>
+                                                    <li><a href="blog.php">Blog</a></li>
+                                                    <li><a href="single-blog.php">Blog Details</a></li>
+                                                    <li><a href="details.php">Categori Details</a></li>
                                                 </ul>
                                             </li>
                                         </ul>
